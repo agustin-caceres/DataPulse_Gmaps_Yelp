fbeb76cce2825b5fd566d9b1a35d735cd630a9ea
Agustín Cáceres <agusca.saot@gmail.com>
2024-10-26 05:56:37 -0300

    Actualización Readme Streamlit

--- Streamlit/components/recommendations.py
+++ Streamlit/components/recommendations.py
@@ -3,6 +3,7 @@
 import streamlit as st
 from utils.helpers import generate_dummy_recommendations
 
+# Muestra las recomendaciones en una fila de tres columnas
 def display_recommendations(user_preferences):
     """
     Módulo para mostrar las recomendaciones generadas a partir de las preferencias del usuario.
@@ -12,15 +13,18 @@ def display_recommendations(user_preferences):
     """
     recommendations = generate_dummy_recommendations(user_preferences)
     if recommendations:
-        for rec in recommendations:
-            with st.container():
-                st.subheader(f"{rec['nombre']} {get_food_icon(rec['tipo_de_comida'])}")
-                st.write(f"📍 Ubicación: {rec['ubicacion']}")
-                st.write(f"🍽️ Tipo de comida: {rec['tipo_de_comida']}")
-                st.write(f"⭐ Calificación: {rec['calificacion']} / 5")
-                st.markdown("<hr style='border-top: 2px solid #79b4b7;'>", unsafe_allow_html=True)  # Línea divisoria
+        for i in range(0, len(recommendations), 3):  # Agrupar de tres en tres
+            cols = st.columns(3)
+            for col, rec in zip(cols, recommendations[i:i+3]):
+                with col:
+                    st.subheader(f"{rec['nombre']} {get_food_icon(rec['tipo_de_comida'])}")
+                    st.write(f"📍 Ubicación: {rec['ubicacion']}")
+                    st.write(f"🍽️ Tipo de comida: {rec['tipo_de_comida']}")
+                    st.write(f"⭐ Calificación: {rec['calificacion']} / 5")
+                    st.markdown("<hr style='border-top: 2px solid #79b4b7;'>", unsafe_allow_html=True)
     else:
         st.write("No se encontraron recomendaciones para las preferencias seleccionadas.")
+
 
 def get_food_icon(food_type):
     """
