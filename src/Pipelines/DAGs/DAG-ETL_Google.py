ed3ab7d2a6117df37f6d311e5d398d538507e8ca
mauricioarcez <mauricioarcez23@gmail.com>
2024-11-04 22:29:48 -0300

    prueba

--- src/Pipelines/DAGs/DAG-ETL_Google.py
+++ src/Pipelines/DAGs/DAG-ETL_Google.py
@@ -1,9 +1,8 @@
 from airflow import DAG
 from airflow.providers.google.cloud.transfers.gcs import GCSCopyObjectOperator
-from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.gcs import GCSListObjectsOperator
 from datetime import timedelta
 from airflow.utils.dates import days_ago
-from google.cloud import storage
 
 #######################################################################################
 # PARÁMETROS
@@ -12,6 +11,7 @@ from google.cloud import storage
 nameDAG_base = 'Transferencia_Todos_Los_Archivos_GCS'
 bucket_source = 'datos-crudos' 
 bucket_destino = 'temporal-procesados'  
+prefix = 'g_sitios/'  # Prefijo para los archivos en el bucket
 
 default_args = {
     'owner': 'Mauricio Arce',
@@ -19,16 +19,6 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
 }
-
-#######################################################################################
-# FUNCIÓN PARA LISTAR ARCHIVOS EN UN BUCKET
-#######################################################################################
-
-def listar_archivos(bucket_name, prefix='g_sitios/'):
-    """Lista todos los archivos en el bucket especificado dentro del prefijo dado y devuelve sus rutas."""
-    client = storage.Client()
-    blobs = client.list_blobs(bucket_name, prefix=prefix)
-    return [blob.name for blob in blobs if not blob.name.endswith('/')]  # Filtrar solo archivos, no directorios
 
 #######################################################################################
 # DEFINICIÓN DEL DAG
@@ -41,11 +31,12 @@ with DAG(
     catchup=False
 ) as dag:
 
-    # Tarea 1: Listar archivos en el bucket de origen
-    listar_archivos_task = PythonOperator(
+    # Tarea 1: Listar archivos en el bucket de origen con el prefijo especificado
+    listar_archivos_task = GCSListObjectsOperator(
         task_id='listar_archivos',
-        python_callable=listar_archivos,
-        op_kwargs={'bucket_name': bucket_source},
+        bucket=bucket_source,
+        prefix=prefix,
+        delimiter='/',
         do_xcom_push=True  # Permitir que la tarea devuelva la lista de archivos
     )
 
@@ -59,7 +50,7 @@ with DAG(
                 source_bucket=bucket_source,
                 source_object=archivo,
                 destination_bucket=bucket_destino,
-                destination_object=archivo,  # Mantiene la misma ruta en el bucket de destino
+                destination_object=archivo,
                 move_object=False  # Cambiar a True si deseas mover los archivos
             )
             transferir_archivo.execute(context=kwargs)
