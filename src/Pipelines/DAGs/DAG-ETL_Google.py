4df33ab5d20c092bfc3d18d2df4b88e8043704fa
mauricioarcez <mauricioarcez23@gmail.com>
2024-11-05 01:41:13 -0300

    modular funciones

--- src/Pipelines/DAGs/DAG-ETL_Google.py
+++ src/Pipelines/DAGs/DAG-ETL_Google.py
@@ -1,3 +1,4 @@
+# Librerias
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
@@ -5,11 +6,14 @@ from datetime import timedelta
 from airflow.utils.dates import days_ago
 from google.cloud import storage
 
-# Parámetros
-nameDAG_base = 'Mover_Archivos_GCS_Subcarpeta'
-bucket_source = 'datos-crudos'
-bucket_destino = 'temporal-procesados'
-subcarpeta = 'g_sitios/'  # Carpeta específica que quieres mover
+# Funciones
+from functions.mover_archivos import mover_archivos
+######################################################## PARAMETROS
+
+nameDAG_base        = 'Mover_Archivos_GCS_Subcarpeta'
+bucket_source       = 'datos-crudos'
+bucket_destino      = 'temporal-procesados'
+subcarpeta          = 'g_sitios/'  
 
 default_args = {
     'owner': 'Mauricio Arce',
@@ -18,24 +22,7 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-# Función para mover archivos dentro de una subcarpeta específica
-def mover_archivos():
-    """Mueve los archivos dentro de una subcarpeta específica de un bucket a otro."""
-    client = storage.Client()
-    bucket_src = client.bucket(bucket_source)
-    bucket_dest = client.bucket(bucket_destino)
-
-    # Listar todos los blobs que estén en la subcarpeta especificada
-    blobs = bucket_src.list_blobs(prefix=subcarpeta)
-
-    for blob in blobs:
-        # Copiar el archivo al bucket destino
-        new_blob = bucket_dest.blob(blob.name)
-        new_blob.rewrite(blob)
-        
-        # Eliminar el archivo del bucket de origen para completar el movimiento
-        blob.delete()
-        print(f"Archivo {blob.name} movido a {bucket_destino} y eliminado de {bucket_source}.")
+######################################################## DAG
 
 # Definición del DAG
 with DAG(
@@ -45,13 +32,13 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    # Tareas Dummy para representar inicio y fin del proceso
     inicio = DummyOperator(task_id='inicio')
 
-    # Tarea para mover archivos
+    # Tarea 1: Mover archivos de un bucket a otro
     mover_archivos_task = PythonOperator(
         task_id='mover_archivos_g_sitios',
         python_callable=mover_archivos,
+        op_args=[bucket_destino, bucket_source, subcarpeta],
     )
     
     fin = DummyOperator(task_id='fin')
