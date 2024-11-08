c0b35650980eb1a3a6965fdc1102516cd4abda4b
Agustín Cáceres <agusca.saot@gmail.com>
2024-11-07 22:41:49 -0300

    actu

--- src/Pipelines/DAGs/Dag_yelp.py
+++ src/Pipelines/DAGs/Dag_yelp.py
@@ -14,7 +14,7 @@ from functions.transform_data_yelp import transformar_checkin, transformar_tip
 # PARÁMETROS PARA DATOS DE YELP
 ######################################################################################
 
-nameDAG_base = 'ETL_yelp'
+nameDAG_base = 'ETL_Yelp'
 project_id = 'neon-gist-439401-k8'
 dataset = '1'
 owner = 'Agustín'
